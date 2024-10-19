b667cd516bea65965702f266187f258ef0d86ab9
Uliana Komarynska <uliakom@gmail.com>
2024-10-19 22:45:47 +0300

    fixing api

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'online-chat-backend-seven.vercel.app',
+  baseURL: 'https://online-chat-frontend-ten.vercel.app',
   // baseURL: 'http://localhost:3001',
   timeout: 10000,
 });
